3a8880fe97df791d61adc2e57f256fc1633f6243
Tomoya6436 <94614689+Tomoya6436@users.noreply.github.com>
2023-08-17 14:53:52 +0900

    delete

--- js/FileSaveAs.js
+++
@@ -1,8 +0,0 @@
-﻿function FileSaveAs(filename, contentType, content) {
-    var link = document.createElement('a');
-    link.download = filename;
-    link.href = "data:" + contentType + ";base64," + content;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
